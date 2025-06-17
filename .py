e7ead83e2f51d1c054a0f4e9f0eda843e37eccd5
manyasa elton <eltonmakachia01@gmail.com>
2025-06-17 14:22:33 +0300

    commit as always

---
+++ .py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Hello, {name}!"
+print(greet("World"))
+# This is a simple greeting function
+# that takes a name as input and returns a greeting message.
+# The function is designed to be easy to use and understand.
+# The function can be extended to include more features in the future.
